749a8de910403172f68d971da35f08a1d399be02
João Leite <joao.leite@isobar.com>
2021-03-16 14:46:30 +0000

    FUNFUNZ-134: playground fixed tslint

--- src/playground.ts
+++ src/playground.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
 import { BlobItem, BlobServiceClient } from '@azure/storage-blob'
 import dotenv from 'dotenv'
 
